1a6d7c1c18624a8a870aa78f5faf9564d6b786cc
daydy2 <igedanielolumide@gmail.com>
2023-05-05 13:55:59 +0100

    fix: css bug 05

--- src/App.jsx
+++ src/App.jsx
@@ -15,12 +15,14 @@ import userSlice from "./store/store";
 import EditPage from "./Pages/EditPage";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { createGlobalStyle } from "styled-components";
+// import { createGlobalStyle } from "styled-components";
 
-const GlobalStyle = createGlobalStyle`
-@import url('https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400;0,500;0,600;0,700;1,500;1,600&display=swap');
-
-`;
+// const GlobalStyle = createGlobalStyle`
+// @import url('https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400;0,500;0,600;0,700;1,500;1,600&display=swap');
+// `;
+{
+  /* <GlobalStyle /> */
+}
 
 const App = () => {
   const isLoggedIn = userSlice((state) => state.isLoggedIn);
@@ -43,48 +45,45 @@ const App = () => {
   }, [isLoggedIn]);
 
   return (
-    <>
-      <GlobalStyle />
-      <div className="App">
-        <header className="landing">
-          <Toolbar drawerClickHandler={drawerToggleClickHandler} />
-          <SideDrawer show={sideDrawerOpen}></SideDrawer>
-          {sideDrawerOpen ? <Backdrop click={backdropClickHandler} /> : null}
-        </header>
-        <ToastContainer />
+    <div className="App">
+      <header className="landing">
+        <Toolbar drawerClickHandler={drawerToggleClickHandler} />
+        <SideDrawer show={sideDrawerOpen}></SideDrawer>
+        {sideDrawerOpen ? <Backdrop click={backdropClickHandler} /> : null}
+      </header>
+      <ToastContainer />
 
-        {isLoggedIn ? (
-          <>
-            <div className="sidebar">
-              <LandingPage />
-            </div>
-
-            <div className="main-area">
-              <div className="feed-area">
-                <Routes>
-                  <Route path="/" element={<Feedlane />} />
-                  <Route path="/profile" element={<ProfilePage />} />
-                  <Route path="/comment/:postId" element={<CommentPage />} />
-                  <Route path="/edit/:postId" element={<EditPage />} />
-                </Routes>
-              </div>
-              <div className="newpost">
-                {/* <Date /> */}
-                <NewPost />
-                {/* <FollowerComp /> */}
-              </div>
-            </div>
-          </>
-        ) : (
-          <div className="no-user">
-            <Routes>
-              <Route path="/register" element={<Signup />} />
-              <Route path="/login" element={<Login />} />
-            </Routes>
+      {isLoggedIn ? (
+        <>
+          <div className="sidebar">
+            <LandingPage />
           </div>
-        )}
-      </div>
-    </>
+
+          <div className="main-area">
+            <div className="feed-area">
+              <Routes>
+                <Route path="/" element={<Feedlane />} />
+                <Route path="/profile" element={<ProfilePage />} />
+                <Route path="/comment/:postId" element={<CommentPage />} />
+                <Route path="/edit/:postId" element={<EditPage />} />
+              </Routes>
+            </div>
+            <div className="newpost">
+              {/* <Date /> */}
+              <NewPost />
+              {/* <FollowerComp /> */}
+            </div>
+          </div>
+        </>
+      ) : (
+        <div className="no-user">
+          <Routes>
+            <Route path="/register" element={<Signup />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </div>
+      )}
+    </div>
   );
 };
 
